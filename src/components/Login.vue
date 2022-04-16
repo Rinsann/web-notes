63823da399f62de10f65d340f5a2c00bb6f28dbd
yume <785879986@qq.com>
2022-04-16 21:58:17 +0800

    封装 axios 统一接口

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,7 +10,7 @@
               <div v-bind:class="{show: isShowRegister}" class="register">
                 <input type="text" v-model="register.username" placeholder="用户名">
                 <input type="password" v-model="register.password" @keyup.enter="onRegister" placeholder="密码">
-                <p v-bind:class="{error: register.isError}"> {{register.notice}}</p>
+                <p v-bind:class="{error: register.isError}"> {{ register.notice }}</p>
                 <div class="button" @click="onRegister">创建账号</div>
               </div>
             </transition>
@@ -18,8 +18,8 @@
             <transition name="slide">
               <div v-bind:class="{show: isShowLogin}" class="login">
                 <input type="text" v-model="login.username" placeholder="输入用户名">
-                <input type="password" v-model="login.password" @keyup.enter="onLogin"  placeholder="密码">
-                <p v-bind:class="{error: login.isError}"> {{login.notice}}</p>
+                <input type="password" v-model="login.password" @keyup.enter="onLogin" placeholder="密码">
+                <p v-bind:class="{error: login.isError}"> {{ login.notice }}</p>
                 <div class="button" @click="onLogin"> 登录</div>
               </div>
             </transition>
@@ -27,75 +27,79 @@
         </div>
       </div>
     </div>
-
-    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data(){
-      return {
-        isShowLogin: true,
-        isShowRegister: false,
-        login: {
-          username: '',
-          password: '',
-          notice: '输入用户名和密码',
-          isError: false
-        },
-        register: {
-          username: '',
-          password: '',
-          notice: '创建账号后，请记住用户名和密码',
-          isError: false
-        }
-      }
-    },
-    methods: {
-      showLogin(){
-        this.isShowLogin = true
-        this.isShowRegister = false
+import request from '@/helpers/request';
+
+request('/auth/login', 'POST', {username: 'hunger', password: '123456'})
+  .then(data => {
+    console.log(data)
+  })
+
+export default {
+  data() {
+    return {
+      isShowLogin: true,
+      isShowRegister: false,
+      login: {
+        username: '',
+        password: '',
+        notice: '输入用户名和密码',
+        isError: false
       },
-      showRegister(){
-        this.isShowLogin = false
-        this.isShowRegister = true
-      },
-      onRegister(){
-        if(!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.register.username)){
-          this.register.isError = true
-          this.register.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
-          return
-        }
-        if(!/^.{6,16}$/.test(this.register.password)){
-          this.register.isError = true
-          this.register.notice = '密码长度为6~16个字符'
-          return
-        }
-        this.register.isError = false
-        this.register.notice = ''
-        console.log(`start register..., username: ${this.register.username} , password: ${this.register.password}`)
-      },
-      onLogin(){
-        if(!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)){
-          this.login.isError = true
-          this.login.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
-          return
-        }
-        if(!/^.{6,16}$/.test(this.login.password)){
-          this.login.isError = true
-          this.login.notice = '密码长度为6~16个字符'
-          return
-        }
-        this.login.isError = false
-        this.login.notice = ''
-        
-        console.log(`start login..., username: ${this.login.username} , password: ${this.login.password}`)      
+      register: {
+        username: '',
+        password: '',
+        notice: '创建账号后，请记住用户名和密码',
+        isError: false
       }
     }
-  }
-</script>
+  },
+  methods: {
+    showLogin() {
+      this.isShowLogin = true
+      this.isShowRegister = false
+    },
+    showRegister() {
+      this.isShowLogin = false
+      this.isShowRegister = true
+    },
+    onRegister() {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.register.username)) {
+        this.register.isError = true
+        this.register.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if (!/^.{6,16}$/.test(this.register.password)) {
+        this.register.isError = true
+        this.register.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.register.isError = false
+      this.register.notice = ''
+      console.log(`start register..., username: ${this.register.username} , password: ${this.register.password}`)
+    },
+    onLogin() {
+      if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
+        this.login.isError = true
+        this.login.notice = '用户名3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if (!/^.{6,16}$/.test(this.login.password)) {
+        this.login.isError = true
+        this.login.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.login.isError = false
+      this.login.notice = ''
 
+      console.log(`start login..., username: ${this.login.username} , password: ${this.login.password}`)
+    }
+  }
+}
+</script>
 
 
 <style lang="less">
@@ -110,10 +114,12 @@
   display: table;
   transition: opacity .3s ease;
 }
+
 .modal-wrapper {
   display: table-cell;
   vertical-align: middle;
 }
+
 .modal-container {
   width: 800px;
   height: 500px;
@@ -124,15 +130,18 @@
   transition: all .3s ease;
   font-family: Helvetica, Arial, sans-serif;
   display: flex;
+
   .main {
     flex: 1;
     background: #36bc64 url(//cloud.hunger-valley.com/17-12-13/38476998.jpg-middle) center center no-repeat;
     background-size: contain;
   }
+
   .form {
     width: 270px;
     border-left: 1px solid #ccc;
     overflow: hidden;
+
     h3 {
       padding: 10px 20px;
       margin-top: -1px;
@@ -140,10 +149,12 @@
       font-size: 16px;
       border-top: 1px solid #eee;
       cursor: pointer;
-      &:nth-of-type(2){
+
+      &:nth-of-type(2) {
         border-bottom: 1px solid #eee;
       }
     }
+
     .button {
       background-color: #2bb964;
       height: 36px;
@@ -155,15 +166,18 @@
       margin-top: 18px;
       cursor: pointer;
     }
-    .login,.register {
+
+    .login, .register {
       padding: 0px 20px;
       border-top: 1px solid #eee;
-       height: 0;
-       overflow: hidden;
-       transition: height .4s;
-       &.show {
+      height: 0;
+      overflow: hidden;
+      transition: height .4s;
+
+      &.show {
         height: 193px;
-       }
+      }
+
       input {
         display: block;
         width: 100%;
@@ -176,21 +190,25 @@
         font-size: 14px;
         margin-top: 10px;
       }
+
       input:focus {
         border: 3px solid #9dcaf8;
       }
+
       p {
         font-size: 12px;
         margin-top: 10px;
         color: #444;
       }
+
       .error {
         color: red;
       }
     }
+
     .login {
       border-top: 0;
-    } 
+    }
   }
 }
 </style>
